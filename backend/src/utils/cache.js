15feaca62e79599dc708c9de74f25c5c7626aa4a
Michał Mrozowski <michal.mrozowski@10clouds.com>
2015-07-27 14:42:21 +0200

    Create 'data' folder if it doesn't exist; Check if results.json is valid

--- backend/src/utils/cache.js
+++ backend/src/utils/cache.js
@@ -5,9 +5,23 @@ var fs      = require('fs');
 var readFile = Promise.promisify(fs.readFile);
 var writeFile = Promise.promisify(fs.writeFile);
 
-var CACHE_FILE = 'data/results.json';
+var CACHE_FOLDER = 'data';
+var CACHE_FILE = CACHE_FOLDER+'/results.json';
+
+var isJsonString = function(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+};
 
 exports.store = co(function* (results) {
+    if (!fs.existsSync(CACHE_FOLDER)){
+        fs.mkdirSync(CACHE_FOLDER);
+    }
+
     try {
         yield writeFile(CACHE_FILE, JSON.stringify(results));
         return true;
@@ -19,6 +33,11 @@ exports.store = co(function* (results) {
 exports.restore = co(function* () {
     try {
         var data = yield readFile(CACHE_FILE, 'utf8');
+
+        if(!isJsonString(data)) {
+            return {};
+        }
+
         return JSON.parse(data);
     } catch(err) {
         return false;
