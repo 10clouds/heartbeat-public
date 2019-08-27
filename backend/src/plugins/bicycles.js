ecccde1609ea673534074a4e55ebeacfcd87ecca
Wiktor Obrebski <wiktor.obrebski@10clouds.com>
2019-08-27 14:11:27 +0200

    Refactor bicycles plugin

--- backend/src/plugins/bicycles.js
+++ backend/src/plugins/bicycles.js
@@ -1,4 +1,3 @@
-const co = require('bluebird').coroutine;
 const request = require('../utils/request.js');
 const logger = require('../utils/logger.js');
 const converter = require('xml-js');
@@ -11,30 +10,29 @@ module.exports = exports = {
     },
     httpRoute: '/bicycles/all',
     refetchInterval: 1000 * 5 * 60,
-    fetchData: co(fetchData)
+    fetchData
 };
 
-function* fetchData() {
-    var closestStationsIds = [9685, 9682, 96821, 9681, 9704],
-        closestStations = [];
+const ClosestStationsIds = [9685, 9682, 96821, 9681, 9704];
 
-    var result = yield request({
+async function fetchData() {
+    const result = await request({
         url: this.options.serverUrl,
         timeout: this.options.timeout
     });
 
-    var response =  converter.xml2json(result[0].body, {compact: true, spaces: 4});
-    var results = JSON.parse(response);
-    var bikeStops = results.markers.country.city.place;
+    const response =  converter.xml2json(result[0].body, {compact: true, spaces: 4});
+    const results = JSON.parse(response);
+    const bikeStops = results.markers.country.city.place;
 
-    bikeStops.forEach(function(elem){
-        if(closestStationsIds.indexOf(parseInt(elem._attributes.number)) > -1){
-            closestStations.push(elem._attributes);
-        }
-    });
+    const closestStations = bikeStops
+        .filter(elem => ClosestStationsIds.includes(parseInt(elem._attributes.number)))
+        .map(elem => elem._attributes);
 
     closestStations.sort(function(a, b){
-        return closestStationsIds.indexOf(parseInt(a.number)) > closestStationsIds.indexOf(parseInt(b.number)) ? 1 : -1
+        const stationId1 = parseInt(a.number);
+        const stationId2 = parseInt(b.number);
+        return ClosestStationsIds.indexOf(stationId1) - ClosestStationsIds.indexOf(parseInt(stationId2));
     });
 
     return closestStations;
